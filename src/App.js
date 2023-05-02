3909befa4ebec01df9c73f20691fde6d6e4438e3
Sibin JOhn <sibinjohn@terrificminds.com>
2023-05-02 17:51:22 +0530

    blog home page template created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import logo from './redfal.png';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-         WELCOME TO REDSKY
-      </header>
-    </div>
+    <>
+<div className="header">
+  <h1><img src={logo} className="App-logo" alt="falcon_logo" />Red Falcons</h1>
+</div>
+
+<div className="menu">
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact Us</a></li>
+  <li><a href="#about">About Us</a></li>
+</ul>
+
+</div>
+
+<div className="main">
+  <h3>Welcome to Red Falcon Club</h3>
+  <p>Here you can read plenty of useful Articles, news and Blogs.</p>
+  <p>Keep supporting and stay with us.</p>
+  <p>Admin - Sibin John</p>
+
+</div>
+    </>
+
   );
 }
 
