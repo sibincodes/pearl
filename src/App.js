5a13c40303f7c55606ad8922f36d45d3b97b0029
Sibin JOhn <sibinjohn@terrificminds.com>
2023-05-03 15:25:56 +0530

    mediq query changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import './App.css';
 import logo from './redfal.png';
 const App = () => {
   return (
-    <>
+    <div className="parent">
 <div className="header">
   <h1><img src={logo} className="App-logo" alt="falcon_logo" />Red Falcons</h1>
 </div>
 
-<div className="menu">
-<ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact Us</a></li>
-  <li><a href="#about">About Us</a></li>
-</ul>
-
+<div className='main-content'>
+<div class="sidebar">
+  <a href="#home"><i class="fa fa-fw fa-home"></i> Home</a>
+  <a href="#services"><i class="fa fa-fw fa-wrench"></i> Services</a>
+  <a href="#clients"><i class="fa fa-fw fa-user"></i> Clients</a>
+  <a href="#contact"><i class="fa fa-fw fa-envelope"></i> Contact</a>
 </div>
 
 <div className="main">
@@ -22,10 +20,12 @@ const App = () => {
   <p>Here you can read useful Articles,</p>
     <p>News and Blogs.</p> 
   <p>Keep supporting and stay with us.</p>
-  <p>Admin - Sibin John</p>
-
 </div>
-    </>
+</div>
+
+
+
+    </div>
 
   );
 }
